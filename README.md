dc1b0258adb2720a125fdb5820c0250689e8f7e4
Himmat Mahal <himmatmahal@outlook.com>
2020-08-23 23:30:57 -0400

    added more functionality

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ by Himmat Mahal
 
 This project will take various formulas for calculting pi,
 and compare how accurate they are.
+
+Program uses matplotlib to visualize graphs created by
+these various formulas.
--- calculate_pi.py
+++ calculate_pi.py
@@ -1,36 +1,56 @@
-
+import matplotlib.pyplot as plt
+import math
 
 # Below are a few functions that calculate pi
-def basel(iterations=10):
+def basel(terms=10):
     # https://en.wikipedia.org/wiki/Basel_problem
     pi_sum = 0.0
-    for i in range(1, iterations+1):
-        pi_sum = pi_sum + (i**(-2))
-
+    for i in range(1, terms):
+        pi_sum += (i**(-2))
     pi_approx = (6*pi_sum)**(0.5)
-    print("{} iterations using Eulers infinite series yields: {}".format(i, pi_approx))
+    return pi_approx
 
-def wallis(iterations=10):
+def wallis(terms=10):
     # https://en.wikipedia.org/wiki/Wallis_product
-    pi_prod = 1.0
-    for i in range(1, iterations+1):
+    pi_product = 1.0
+    for i in range(1, terms):
         n = float(i)
-        pi_prod = pi_prod*( ((2*n)**2) /( ((2*n)-1)*((2*n)+1) ) )
+        pi_product = pi_product*( ((2*n)**2) /( ((2*n)-1)*((2*n)+1) ) )
+    pi_approx = 2*pi_product
+    return pi_approx
 
-    pi_approx = 2*pi_prod
-    print("{} iterations using Wallis product yields: {}".format(i, pi_approx))
-
-def leibniz_madhava(iterations=10):
+def leibniz_madhava(terms=10):
     # https://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80
     pi_sum = 0.0
-    for i in range(1, iterations+1):
+    for i in range(1, terms):
         n = (2*i - 1)*((-1)**(i+1))
-        pi_sum = pi_sum + 1/n
-
+        pi_sum += 1/n
     pi_approx = 4*pi_sum
-    print("{} iterations using Leibniz-Madhava formula yields: {}".format(i, pi_approx))
+    return pi_approx
 
+def odd_product_formula(terms=10):
+    # https://en.wikipedia.org/wiki/List_of_formulae_involving_%CF%80 - under 'other infinite series'
+    pi_sum = 0.00
+    for i in range(terms):
+        n = ((4*i)+1)*((4*i)+3)
+        pi_sum += 1/n
+    pi_approx = 8*pi_sum
+    return pi_approx
 
-basel(10000)
-wallis(10000)
-leibniz_madhava(10000)
+ending_point = int(input("Enter the number of terms at which to stop the formulas at: "))
+starting_point = int(input("Enter the number of terms to start comparing formulas: "))
+
+# creating lists of approximations of pi based on starting and ending points
+list_one = [odd_product_formula(x) for x in range(starting_point, ending_point)]
+list_two = [leibniz_madhava(x) for x in range(starting_point, ending_point)]
+list_three = [wallis(x) for x in range(starting_point, ending_point)]
+list_four = [basel(x) for x in range(starting_point, ending_point)]
+pi_constant = [math.pi for x in range(starting_point, ending_point)]
+
+list_of_plots = [list_one, list_two, list_three, list_four, pi_constant]
+
+# creates a graphical plot of each formula
+for list in list_of_plots:
+    plt.plot(list)
+
+plt.show()
