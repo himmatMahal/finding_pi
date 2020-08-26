d6bfca218cce0c9a1cfdff557b048891395137dc
Himmat Mahal <himmatmahal@outlook.com>
2020-08-26 00:13:24 -0400

    updated gui

--- using_gui_with_plot.py
+++ using_gui_with_plot.py
@@ -10,14 +10,16 @@ class VisualizingPi:
         self.HEIGHT = 600
         self.WIDTH = 800
         self.RADIOBUTTON_FONT = ("Helvetica", 14, "bold")
-
-        self.button_start_var = tk.IntVar()
-        self.button_end_var = tk.IntVar()
+        self.LABEL_FONT = ("Helvetica", 12,)
 
         self.start_buttons = [None]*5
         self.starting_options = [0,1,10,100,1000]
         self.end_buttons = [None]*5
         self.ending_options = [5,10,25,500,5000]
+
+        self.button_start_var = tk.IntVar()
+        self.button_end_var = tk.IntVar(value=self.ending_options[-1])
+        # by default, start is 0 and end is maximum
 
         master.title('Visualizing Pi')
         self.canvas = tk.Canvas(master, height=self.HEIGHT, width=self.WIDTH, bg='#2c2c4d')
@@ -43,21 +45,40 @@ class VisualizingPi:
             button.place(relx=0.1,rely=y_position)
             i=i+1
 
-    # functions to set starting and ending points:
-    def set_start(self):
-        print("{} was selected (start)".format(self.button_start_var.get()))
+        # adding labels to display current selected points
+        self.start_label = tk.Label(self.canvas, text='Starting \npoint \nselected: {}'.format(self.button_start_var.get()),
+                                    font = self.LABEL_FONT)
+        self.end_label = tk.Label(self.canvas, text='Ending \npoint \nselected: {}'.format(self.button_end_var.get()),
+                                    font = self.LABEL_FONT)
+        self.start_label.place(relx=0.1, rely=0.7, relwidth=0.14, relheight=0.12)
+        self.end_label.place(relx=0.3, rely=0.7, relwidth=0.14, relheight=0.12)
 
+        # adding buttons to toggle plots
+        self.show_plots = tk.Button(self.canvas, text='show plots', command=self.create_plots)
+        self.show_plots.place(relx=0.1, rely=0.85, relwidth = 0.34)
+
+
+    # methods to set starting and ending points, will be used when radio buttons are clicked:
+    def set_start(self):
+        self.start_label.configure(text='Starting \npoint \nselected: {}'.format(self.button_start_var.get()))
     def set_end(self):
-        print("{} was selected (end)".format(self.button_end_var.get()))
+        self.end_label.configure(text='Ending \npoint \nselected: {}'.format(self.button_end_var.get()))
         i=0
         for x in self.starting_options:
             if self.button_end_var.get() <= x:
                 self.start_buttons[i].configure(state='disabled')
                 if self.button_start_var.get()>=self.button_end_var.get():
                     self.start_buttons[0].select()
+                    self.start_label.configure(text='Ending \npoint \nselected: {}'.format(self.button_start_var.get()))
             else:
                 self.start_buttons[i].configure(state='normal')
             i=i+1
+
+    # methods to create and destroy graphs, based on which button user clicks
+    def create_plots(self):
+        points = range(self.button_start_var.get(), self.button_end_var.get())
+        plt.plot(points)
+        plt.show()
 
 
 def main():
